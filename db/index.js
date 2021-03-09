26733239bf396f224ac78f41ac939edb7e0e7b32
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-03-09 18:43:34 -0500

    Connecting to the database

---
+++ db/index.js
@@ -0,0 +1,19 @@
+const pg = require('pg');
+
+const client = new pg.Client({
+    host: 'localhost',
+    database: 'wnews',
+    port: 5432,
+});
+
+//connect takes a callback function in the event there is an error
+//you want to handle
+client.connect(err => {
+    if(err){
+        console.error('connection error!', err.stack)
+    } else {
+        console.log('Connected to PostgreSQL on port', client.port)
+    }
+})  
+
+module.exports = client;
