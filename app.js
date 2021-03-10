53fa0366184bba023e1d82a6de5bbc4aac8e0253
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-03-09 19:33:27 -0500

    The Big Step

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ const morgan = require("morgan");
 const postBank = require("./postBank");
 const postList = require("./views/postList");
 const postDetails = require("./views/postDetails");
+const client = require("./db");
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  const posts = postBank.list();
-  res.send(postList(posts));
+app.get("/", async (req, res) => {
+  try {
+    const data = await client.query('SELECT * FROM posts INNER JOIN users ON posts.userid = users.id');
+    const posts = data.rows;
+    res.send(postList(posts));
+  } catch (error) {
+    console.error('Error occured:', error);
+  }
 });
 
-app.get("/posts/:id", (req, res) => {
-  const post = postBank.find(req.params.id);
-  res.send(postDetails(post));
+app.get("/posts/:id", async (req, res) => {
+  try {
+    const data = await client.query('SELECT * FROM posts INNER JOIN users ON posts.userid=users.id WHERE posts.id=$1', [req.params.id])
+    const post = data.rows[0];
+    res.send(postDetails(post));
+  } catch (error) {
+    console.error('Error occured:', error);
+  }
 });
 
 const PORT = 1337;
